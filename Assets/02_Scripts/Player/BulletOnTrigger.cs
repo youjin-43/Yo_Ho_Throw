ab2b9c281a8b16e63b0d101463ab80397a1c1233
김석진 <69078017+eeptmxlsl15@users.noreply.github.com>
2025-02-22 20:53:20 +0900

    점프 수정 , 불릿 위치 수정 , 즉시 점프 할 수 있게 변경
    점프 수정 : 껐다 키니까 해결됨
    불릿 위치 수정 : 투척 애니메이션에 맞게 수정
    즉시 점프 할 수 있게 수정 : 착지 이후 바로 점프할 수 있게 됨

--- Assets/02_Scripts/Player/BulletOnTrigger.cs
+++ Assets/02_Scripts/Player/BulletOnTrigger.cs
@@ -5,16 +5,20 @@ public class BulletOnTrigger : MonoBehaviour
     
     private void OnTriggerEnter(Collider other)
     {
+        
         //TODO 팀 구분 해야함 
-        if (!other.CompareTag("Player"))
+        if (other.CompareTag("Player"))
         {
-            Debug.Log("Enemy hit");
-            PlayerManager pc = other.GetComponent<PlayerManager>();
-            pc.OnDamaged(1);
-            PoolManager.Instance.Push(gameObject);
-            Debug.Log("Anything hit");
-        }
+            
+            PlayerManager pc;
+            if (pc = other.GetComponent<PlayerManager>()){
+                pc.OnDamaged(1);
 
+            }
+            
+        }
+        PoolManager.Instance.Push(gameObject);
+        
     }
 
     
