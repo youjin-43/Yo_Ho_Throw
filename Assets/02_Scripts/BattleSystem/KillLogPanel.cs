a5d52786b4466dcc1a2facb062cd20d434928b6e
cksrb4309 <rlacksrb0856@gmail.com>
2025-02-24 02:57:41 +0900

    처치 로그 기능 구현
    추가한 스크립트
     - KillLogPanel : 가해자와 피해자의 이름 저장 및 시간에 따른 Alpha 값 조절 기능
     - KillLogPanelController : Panel 관리 스크립트
    
    추가한 이미지
     - KillLogPanel ( 왼쪽과 오른쪽의 알파 값만 흐린 이미지 )
    
    삭제한 이미지
     - AlphaMask ( 가운데만 알파값이 0인 이미지 / 테스트 용도라서 삭제)
    
    수정한 애니메이션
     - AlphaMaskFadeIn/Out 애니메이션 수정 : AlphaMaskImage의 width 및 height 최대값 2350으로 수정

---
+++ Assets/02_Scripts/BattleSystem/KillLogPanel.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class KillLogPanel : MonoBehaviour
+{
+    [SerializeField] TMP_Text killerNicknameText;
+    [SerializeField] TMP_Text victimNicknameText;
+
+    KillLogPanel front = null;
+    KillLogPanel back = null;
+
+    CanvasGroup canvasGroup = null;
+
+    private void Awake()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+    public void MoveUp(float positionY, float padding)
+    {
+        if (transform.position.y < positionY)
+        {
+            float distance = positionY - transform.position.y;
+
+            transform.position += new Vector3(0, distance, 0);
+
+            back?.MoveUp(transform.position.y + padding, padding);
+        }
+    }
+    public void SetText(string killerNickname, string victimNickname)
+    {
+        killerNicknameText.text = killerNickname;
+        victimNicknameText.text = victimNickname;
+
+        killerNicknameText.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, killerNicknameText.preferredWidth);
+        victimNicknameText.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, victimNicknameText.preferredWidth);
+
+        canvasGroup.alpha = 0;
+
+        StartCoroutine(FadeCoroutine());
+    }
+    public void ClearFrontBack()
+    {
+        if (front != null) front.back = null;
+    }
+    public void SetFront(KillLogPanel panel) => front = panel;
+    public void SetBack(KillLogPanel panel) => back = panel;
+    IEnumerator FadeCoroutine()
+    {
+        float alpha = 0f;
+
+        while (alpha < 1f)
+        {
+            alpha += Time.deltaTime * 5f;
+
+            canvasGroup.alpha = alpha;
+
+            yield return null;
+        }
+        alpha = 1f;
+
+        yield return new WaitForSeconds(1f);
+
+        while (alpha > 0f)
+        {
+            alpha -= Time.deltaTime * 5f;
+
+            canvasGroup.alpha = alpha;
+
+            yield return null;
+        }
+        KillLogPanelController.ReturnPanel(this);
+    }
+}
