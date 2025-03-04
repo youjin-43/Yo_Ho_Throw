0613844d781f6faf14cf70e1ccb7e9aea82702e7
cksrb4309 <rlacksrb0856@gmail.com>
2025-03-05 02:06:28 +0900

    UI 연결 오류 및 캐릭터 킬 오류 해결

--- Assets/02_Scripts/EditPlayer/EditPlayerState.cs
+++ Assets/02_Scripts/EditPlayer/EditPlayerState.cs
@@ -13,17 +13,9 @@ public class EditPlayerState : MonoBehaviourPun, IDamagable
 
         set
         {
-            if (hp > value)
-            {
-                for (; hp == value; hp--)
-                    InGameUIManager.Instance.OnHealthChangedDebug(-1);
-            }
-            else if (hp < value)
-            {
-                for (; hp == value; hp++)
-                    InGameUIManager.Instance.OnHealthChangedDebug(1);
-            }
             hp = value;
+
+            InGameUIManager.Instance.HealthIndicator.SetHealth(hp);
         }
     }
 
@@ -32,38 +24,26 @@ public class EditPlayerState : MonoBehaviourPun, IDamagable
     [PunRPC]
     public void ReceiveDamage(int attackerActorNr, int damage)
     {
-        if (isInLobby) return;
-
-        Debug.Log("맞은 플레이어 : " + photonView.OwnerActorNr.ToString());
-
-        Debug.Log("맞기 전 HP : " + Hp.ToString());
+        if (isInLobby || photonView.IsMine) return;
 
         Hp -= damage;
 
-        Debug.Log("맞은 후 HP : " + Hp.ToString());
-
-        if (Hp <= 0 && photonView.IsMine)
+        if (Hp <= 0)
         {
             photonView.RPC("HandleDeath", RpcTarget.All, attackerActorNr);
-
-            HandleDeath(attackerActorNr);
         }
     }
     [PunRPC]
     void HandleDeath(int killerActorNr)
     {
+        if (!photonView.IsMine) return;
+
         gameObject.name += Random.value.ToString();
-        Debug.Log("죽은 플레이어 : " + gameObject.name);
 
         // 이동 비활성화
         EditPlayerController.Instance.DisableMovement();
 
-        //BattleSystem.Instance.RegisterKill(killerActorNr, photonView.OwnerActorNr);
         BattleSystem.Instance.photonView.RPC("RegisterKillRPC", RpcTarget.All, killerActorNr, photonView.OwnerActorNr);
-
-        Debug.Log("----------HandleDeath----------");
-
-        //BattleSystem.Instance.RegisterKill(this.photonView.OwnerActorNr, photonView.OwnerActorNr);
     }
 
     public void OnInLobby() => isInLobby = true;
