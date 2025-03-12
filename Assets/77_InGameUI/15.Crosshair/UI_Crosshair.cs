b1f364727465d4facd385338ed340ab902faa7ee
hjlee5982 <hjlee5982@naver.com>
2025-03-13 01:18:19 +0900

    조준선에 부딪히면 플레이어 이름이 나와요

---
+++ Assets/77_InGameUI/15.Crosshair/UI_Crosshair.cs
@@ -0,0 +1,97 @@
+using Photon.Pun;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class UI_Crosshair : UI_Base
+{
+    #region VARIABLES
+    private TextMeshProUGUI _outlineText;
+    private TextMeshProUGUI _innerText;
+
+    private Ray _ray;
+    #endregion
+
+
+
+
+
+    #region OVERRIDE
+    public override void Init()
+    {
+        _name = name;
+    }
+
+    public override void On()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public override void Off()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public override void ResetUI()
+    {
+
+    }
+    #endregion
+
+
+
+
+
+    #region BEHAVIOURS
+    void Awake()
+    {
+        _outlineText = transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+        _innerText = transform.GetChild(1).GetComponent<TextMeshProUGUI>();
+
+        _outlineText.gameObject.SetActive(false);
+        _innerText.gameObject.SetActive(false);
+
+    }
+
+    void Update()
+    {
+        Raycasting();
+    }
+    #endregion
+
+
+
+
+
+    #region FUNCTION
+    private void Raycasting()
+    {
+        _ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+
+        if (Physics.Raycast(_ray, out RaycastHit hit, 500f))
+        {
+            Debug.Log(hit.collider.name);
+
+            if (hit.collider.CompareTag("Player"))
+            {
+
+                _outlineText.gameObject.SetActive(true);
+                _innerText.gameObject.SetActive(true);
+
+                // _outlineText.text = 플레이어 아이디 가져오는 함수가 필요할듯
+                // _innerText  .text = 플레이어 아이디 가져오는 함수가 필요할듯
+            }
+            else
+            {
+                _outlineText.gameObject.SetActive(false);
+                _innerText.gameObject.SetActive(false);
+            }
+        }
+    }
+    #endregion
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(_ray.origin, _ray.direction * 100);
+    }
+}
