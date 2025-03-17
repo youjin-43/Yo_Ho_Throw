b422bdab01bc77868d76d03cf6c84b492f909d29
HyeonxJ <b.bhj0817@gmail.com>
2025-03-17 15:44:20 +0900

    코인 버그 해결
    갑자기 상자 오류

--- Assets/02_Scripts/TreasureChest/Coin.cs
+++ Assets/02_Scripts/TreasureChest/Coin.cs
@@ -3,15 +3,15 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider other)
     {
-        if (collision.gameObject.CompareTag("Player")) // 플레이어와 충돌 시
+        if (other.CompareTag("Player")) // 플레이어와 충돌 시
         {
-            Debug.Log("충돌발생");
-            PlayerController playerController = collision.gameObject.GetComponent<PlayerController>();
+            PlayerController playerController = other.GetComponent<PlayerController>();
             if (playerController != null)
             {
                 playerController.AddCoin(1); // 코인 추가
+                Debug.Log("코인획득");
                 PhotonNetwork.Destroy(gameObject); // 네트워크에서 코인 삭제
             }
         }
