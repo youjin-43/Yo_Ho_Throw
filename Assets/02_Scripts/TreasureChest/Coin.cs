01bcdee53df6bcbb366e40bed9dabca58b6e27ba
HyeonxJ <b.bhj0817@gmail.com>
2025-03-17 16:33:06 +0900

    버그 모두모두 해결!~
    -하늘에서 보물상자가 안내려오는 버그
    -코인 획득 시, 소유권 주장 오류 버그
    -보물상자 처음 피격 시 포지션 달라지는 버그

--- Assets/02_Scripts/TreasureChest/Coin.cs
+++ Assets/02_Scripts/TreasureChest/Coin.cs
@@ -1,8 +1,18 @@
-﻿using Photon.Pun;
+﻿using ExitGames.Client.Photon;
+using Photon.Pun;
+using Photon.Realtime;
 using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
+    [SerializeField] TreasureManager treasureManager;
+    private PhotonView photonView;
+
+    private void Start()
+    {
+        treasureManager = FindAnyObjectByType<TreasureManager>();
+        photonView = GetComponent<PhotonView>();
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player")) // 플레이어와 충돌 시
@@ -12,7 +22,16 @@ public class Coin : MonoBehaviour
             {
                 playerController.AddCoin(1); // 코인 추가
                 Debug.Log("코인획득");
-                PhotonNetwork.Destroy(gameObject); // 네트워크에서 코인 삭제
+
+                // 소유권 확인 후 삭제
+                if (PhotonNetwork.IsMasterClient)
+                {
+                    PhotonNetwork.Destroy(gameObject); // 마스터 클라이언트일 때, 코인 삭제
+                }
+                else
+                {
+                    PhotonNetwork.RaiseEvent(1, photonView.ViewID, RaiseEventOptions.Default, SendOptions.SendReliable);
+                }
             }
         }
     }
