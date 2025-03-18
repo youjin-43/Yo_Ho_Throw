42cead6a2791ae17f233816e32d1d29180135b62
hjlee5982 <hjlee5982@naver.com>
2025-03-18 14:53:45 +0900

    상점 안열리는거 수정

--- Assets/77_InGameUI/11_ItemStore/UI_ItemStore.cs
+++ Assets/77_InGameUI/11_ItemStore/UI_ItemStore.cs
@@ -61,7 +61,7 @@ public class UI_ItemStore : UI_Base
         // 맨 처음은 우선 구매를 제한함
         PurchaceDeActivation();
 
-        gameObject.SetActive(false);
+        gameObject.SetActive(true);
     }
     #endregion
 
