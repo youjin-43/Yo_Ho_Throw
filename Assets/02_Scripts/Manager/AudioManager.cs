574a8f199e842e819070e980c83c2b2b269de808
hjlee5982 <hjlee5982@naver.com>
2025-03-18 11:56:26 +0900

    카운트다운 효과음 정상화

--- Assets/02_Scripts/Manager/AudioManager.cs
+++ Assets/02_Scripts/Manager/AudioManager.cs
@@ -24,7 +24,7 @@ public class AudioManager : MonoBehaviourPun
 
         PlayerDead, PlayerKill, PlayerHit, PlayerAttack, PlayerMelee, PlayerWalk, PlayerJump, PlayerDash, PlayerRevive,
 
-        Knife, Block, Bounty, CountDown
+        Knife, Block, Bounty, Countdown_Low, Countdown_High
     }
 
     void Awake()
